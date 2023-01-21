3ccbcb84dcdca5507b1f43b2a2840566194bb168
ThiccPan <ihilmiderian@gmail.com>
2023-01-21 18:58:25 +0700

    add book migration

---
+++ database/migrations/2023_01_21_101901_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('book_author')->nullable();
+            $table->string('book_genre')->nullable();
+            $table->foreign('book_author')->references('id')->on('authors');
+            $table->foreign('book_genre')->references('genre')->on('genres');
+            $table->integer('stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
