0461640783927160834c8524717feec0eb42f764
ThiccPan <ihilmiderian@gmail.com>
2023-01-21 18:59:29 +0700

    add api route and book creat func in controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('/genre', GenreController::class);
 Route::apiResource('/author', AuthorController::class);
+Route::apiResource('/book', BookController::class);
